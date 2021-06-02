e1bfde2e1e5fa92762e8703624668eabb7fb14a1
prem-raj461 <rajprem451@gmail.com>
2021-06-02 10:05:52 +0545

    day 2 completed

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int day = 30;
+  final String name = "prem raj dhungana";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First App"),
+      ),
+      body: Center(
+        child: Text(
+          "welcome to the flutter $day course free by $name",
+          style: TextStyle(color: Colors.green),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:learningflutter/homepage.dart';
 
- void main() {
-   runApp(MyApp()) ;
- }
+void main() {
+  runApp(MyApp());
+}
 
- class MyApp extends StatelessWidget {
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       home: Center(child: Material(child: Text("welcome to the flutter 30 days course free", style: TextStyle(color: Colors.green),),)),
-       
-     );
-   }
- }
+class MyApp extends StatelessWidget {
+  // int day = 30;
+  // String name = "Hi, i am prem raj dhungana";
+  // bool isMale = true; //it can take value in the form of true or false
+  // num val = 3;
+  // var day = "this is sunday"; //automatically idetifies the type of variable
+  // const pi = 3.123; //cannot change the value of pi in later
+  // final pi_value = 3.14; //we can modify the value of pi later
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
